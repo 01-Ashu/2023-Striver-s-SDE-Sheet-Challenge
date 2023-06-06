74028bf16cd657ae0c320a6f426157f8ec311130
01-Ashu <108317308+01-Ashu@users.noreply.github.com>
2023-06-06 15:06:54 +0530

    Update 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col=1;
+        int row=1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        //Row
+        for(int i=0;i<m;i++){
+            if(matrix[0][i]==0){
+                row=0;
+            }
+        }
+        //Col
+          for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                col=0;
+                // cout<<col;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){
+                      matrix[0][j]=0;
+                      matrix[i][0]=0;
+
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                 if(matrix[i][0]==0 || matrix[0][j]==0){
+                     matrix[i][j]=0;
+
+                 }
+            }
+        }
+        if(row==0){
+             for(int i=0;i<m;i++){
+            matrix[0][i]=0;
+                
+            
+        } 
+        }
+        if(col==0){
+             for(int i=0;i<n;i++){
+            matrix[i][0]=0;
+       
+        }
+        }
+    }
+};
